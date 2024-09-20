fc20697c01f0dbfb0e143e061b88298f4a1cffaa
Jonathan Saxon <JonathanSaxon@sthelens.gov.uk>
2024-09-20 10:40:33 +0100

    Initial Code Import

---
+++ app.js
@@ -0,0 +1,69 @@
+let express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const sql = require('mssql');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+
+let allowedOrigins = [
+    "http://localhost:3000"
+];
+
+app.use(
+    cors({
+        origin: function(origin, callback) {
+            if(!origin) return callback(null, true);
+            if(allowedOrigins.indexOf(origin) === -1) {
+                let msg = 
+                    `The CORS policy for this site does not
+                    allow access from the specified Origin.`;
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        }
+    })
+);
+
+app.use(function (req, res, next) {
+    let origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header("Access-Control-Allow-Origin", origin); // restrict it to the required domain
+    }
+
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
+app.use(bodyParser.json({limit: '50mb'}));
+
+const config = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_NAME,
+    options: {
+        encrypt: true
+    }
+};
+
+sql.connect(config, (err) => {
+    if(err) {
+        console.error('Database connection failed:', err);
+    } else {
+        console.log('Connected to the database');
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Helen Connect server is operational');
+});
+
+app.listen(process.env.PORT || PORT, function () {
+    console.log("App listening on", PORT);
+});
