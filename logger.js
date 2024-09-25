dbc088c26a6164cd7154149bad9cc4473d90fff5
jonsaxon77 <jonsaxon@outlook.com>
2024-09-25 19:17:18 +0100

    Added storage logs

--- logger.js
+++ logger.js
@@ -1,14 +1,32 @@
 const winston = require("winston");
+import { winstonAzureBlob, extensions } from "winston-azure-blob";
 
 const logger = winston.createLogger({
   level: "info",
-  format: winston.format.simple(),
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.splat(),
+    winston.format.json()
+  ),
   transports: [
+    winstonAzureBlob({
+      account: {
+        host: "",
+        sasToken: "",
+      },
+      blobName: "helenconnect-error",
+      bufferLogSize: 1,
+      containerName: "logs",
+      level: "error",
+      rotatePeriod: "YYYY-MM-DD",
+      syncTimeout: 0
+    }),
     new winston.transports.File({ filename: "helen-connect.log" }),
     new winston.transports.File({
       filename: "helen-connect-error.log",
       level: "error",
     }),
+    w,
   ],
 });
 
