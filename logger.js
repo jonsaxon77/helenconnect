6970bc5f9d975216f087c4ef8e36bfa79ae494be
jonsaxon77 <jonsaxon@outlook.com>
2024-09-26 07:27:40 +0100

    Fixed Surname

--- logger.js
+++ logger.js
@@ -1,16 +1,36 @@
 const winston = require("winston");
-
+const azureBlobTransport  = require("winston3-azureblob-transport");
 
 const logger = winston.createLogger({
   level: "info",
-  format: winston.format.simple(),
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.splat(),
+    winston.format.json()
+  ),
   transports: [
+
+    new (azureBlobTransport)({
+      account: {
+        name: "gosspoc",
+        key: process.env.STORAGE_KEY
+      },
+      containerName: "logs",
+      blobName: "error_log",
+      level: "error",
+      bufferLogSize : 1,
+      syncTimeout : 0,
+      rotatePeriod : "YYYY-MM-DD",
+      eol : "\n"
+    })
+
+    /*
     new winston.transports.File({ filename: "helen-connect.log" }),
     new winston.transports.File({
       filename: "helen-connect-error.log",
       level: "error",
     }),
-    
+    */
   ],
 });
 
